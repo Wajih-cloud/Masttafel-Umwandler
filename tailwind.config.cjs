ec3f1cbeb7110aa59f3527d854ecbdd1a328e384
Wajih-cloud <wajih.tf@icloud.com>
2025-09-03 13:08:49 +0200

    Add Vite React Tailwind to-do app

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        turquoise: '#06b6d4',
+        ocean: '#3b82f6',
+      },
+    },
+  },
+  plugins: [],
+}
